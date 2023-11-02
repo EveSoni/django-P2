186bae138efcc8e98fcb1126867d046a0cd06f27
EveSoni <mevelyne051@gmail.com>
2023-11-02 12:37:58 +0300

    project 2

---
+++ ITAL/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def blog(request):
+    return render(request, 'blog.html')
+
+
+def coffees(request):
+    return render(request, 'coffees.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
